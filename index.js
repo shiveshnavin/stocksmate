4107547cf0d08c98b0bb5bcbd9cb9a64ece01114
Shivesh Navin <shiveshnavin@gmail.com>
2022-01-07 10:58:37 +0530

    add my stratergy

--- index.js
+++ index.js
@@ -12,22 +12,65 @@ let bCalc = require('./brokerage')
 
 /***************CONFIG***************/
 let Z_USERID = "AMC939"
-let enctoken = "DSDW8Nh+v8PNG15K9CpuAhEWpyysFHTx2/j0VEAHc+37SAdgwzFM8amCgl0wiY08sUuoxHmTWcDAIdKlBnNQQK+vq/GAvrzmfq/41hqkt+gFDroV4UPlqg=="
+let enctoken = "/5yidvI2GWMwI3nnLy7355BTnsBumsgNSUfJGxpc5yowwu4B0nMgN3ZKwjjWKlZHd8Dg+Gb853eqyCR2n2uQlDuvvIh+5Bb++WC4nCFd3FUDhaEchgMJaw=="
 let usablableBalance = function (balance) {
-    return balance * 0.9;
+    return balance;
 }
-let START_HRS = 9, START_MINS = 15
+let START_HRS = 9, START_MINS = 15;
+let STOP_HRS = 15, STOP_MINS = 10
+
 let trade = 'MIS'
 let TARGET_PROF_PER_SHARE = 10;
 let BUY_AT_MAX_FROM_PREV_DAY = 1;
-let FORCE_BUY = true;
-let calculator = trade == 'MIS' ? bCalc.cal_intra : bCalc.cal_delivery
-/******************************/
+let FORCE_BUY = false;
+let FORCE_SELL = false;
 
+let calculator = trade == 'MIS' ? bCalc.cal_intra : bCalc.cal_delivery
+/*****************************
+
+-----------------------------
+My Simple stratergy is to:  |
+-----------------------------
+HALT TRADING FOR DAY IF ASIAN MARKET ENDS UP IN RED OR ANY BAD NEWS/MANUAL OVERRIDE
+Get the CLOSING_PRICE of prev day or before market open price today and 
+get the CASH_MARGIN with me
+place the order to buy at prev day CLOSING_PRICE + 1 with QTY as my CASH_MARGIN * 0.9
+wait till the order is open . in case order is cancelled , keep repeating the order to buy
+if order is open. wait for order to be executed
+when order is executed . place an order to sell at limit + X (say X=7rs) amount based on profit
+
+>>> This stratergy can incur huge losses if market has a gap down opening
+
+-----------------------------
+My Backup stratergy is to:  |
+-----------------------------
+HALT TRADING FOR DAY IF ASIAN MARKET ENDS UP IN RED OR ANY BAD NEWS/MANUAL OVERRIDE
+Get the CLOSING_PRICE of prev day or before market open price today = LIMIT_BUY_PRICE 
+get the CASH_MARGIN with me
+place the order to buy at prev day LIMIT_BUY_PRICE + 0.5 with QTY as my CASH_MARGIN * 0.45
+wait till the order is open . in case order is cancelled , keep repeating the order to buy
+if order is open. wait for order to be executed
+when order is executed and shares are bought at ACTUAL_BUY_PRICE. 
+place an order to sell at SELL_PRICE = limit + X (say X=7rs) amount based on profit
+keep watching the MARKET_PRICE 
+in case price falls say at BACKUP_PRICE = ACTUAL_BUY_PRICE - 10
+Make an educated guess between:
+1. Whether selling complete QTY at BACKUP_PRICE 
+   and buying QTY at BACKUP_PRICE and selling at 
+    BACKUP_PRICE +  { 0, ACTUAL_BUY_PRICE + ACTUAL_BREAK_EVEN, BACKUP_PRICE - 5,BACKUP_PRICE + 5}
+2. Whether buying at BACKUP_PRICE QTY/2 and selling at
+    BACKUP_PRICE +  { 0, ACTUAL_BUY_PRICE + ACTUAL_BREAK_EVEN, BACKUP_PRICE - 5,BACKUP_PRICE + 5}
+
+which is benificial ? and do that . and place an order for
+     BACKUP_SELL_PRICE =  ACTUAL_BUY_PRICE + ACTUAL_BREAK_EVEN
+
+If MARKET_PRICE dosent hit the 
+
+*****************************/
 
 
 process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise', 'reason:', reason.response.data.message, reason.stack);
+    console.log('Unhandled Rejection at: Promise', 'reason:', reason.response ? reason.response.data.message : reason.message, reason.stack);
 });
 
 async function start(symbol) {
@@ -49,7 +92,7 @@ async function start(symbol) {
     let lastClose = lastDay.close;
 
     // try to buy before 9:15
-    await tryToPlaceLimitOrderBefore915(lastClose + BUY_AT_MAX_FROM_PREV_DAY, symbol)
+    await tryToPlaceLimitOrderBefore915(1544.1, symbol)
 
 
 
@@ -79,16 +122,18 @@ async function tryToPlaceLimitOrderBefore915(limitPrice, symbol) {
                     let brokerage = calculator(limitPrice, targetSell, qty, true)
                     let brokerageMin = calculator(limitPrice, limitPrice, qty, true)
 
-                    let brokerageInfo = `[Total Charges ${brokerage.total_tax} Target Breakeven ${brokerage.breakeven} Target Profit ${brokerage.net_profit} | Min Breakeven ${brokerageMin.breakeven}]`
+                    let brokerageInfo = `[Total Charges ${brokerage.total_tax} Min Breakeven +${brokerage.breakeven} Target Profit ${brokerage.net_profit}]`
 
                     console.log('Trying to place a buy order before market opens @', limitPrice, 'x', qty, '=', requiredMargin ? requiredMargin.total : `[${qty * marginForOneQty.total}]`, brokerageInfo)
 
-                    let orderPlaceResult = await tryToPlaceOrderZerodha(limitPrice, qty, 'BUY', symbol)
+                    let orderPlaceResult = await tryToPlaceOrderZerodha(limitPrice, 31, 'BUY', symbol, 'LIMIT')
                     if (orderPlaceResult.ok) {
+                        await waitTillOrderIsOpen(orderPlaceResult.order_id)
                         console.log('-------------ORDER PLACED----------', orderPlaceResult)
-                        let sell = await tryToPlaceOrderZerodha(targetSell, qty, 'SELL', symbol)
+                        let sell = await tryToPlaceOrderZerodha(targetSell, qty, 'SELL', symbol, 'LIMIT')
                         if (sell.ok) {
-                            console.log('-------------SELL ORDER PLACED---------- target @', targetSell)
+                            console.log('-------------SELL ORDER PLACED---------- target @', targetSell, sell)
+                            // startMarketWatch()
                         }
                         else {
                             console.log('!!!!!! SELL ORDER NOT PLACED !!!!!! Please do manually, target @', targetSell)
@@ -117,10 +162,11 @@ async function tryToPlaceLimitOrderBefore915(limitPrice, symbol) {
  * @param {*} symbol 
  * @returns 
  */
-async function tryToPlaceOrderZerodha(limitPrice, qty, type, symbol) {
-    var data = `variety=regular&exchange=NSE&tradingsymbol=${symbol}&transaction_type=${type}&order_type=LIMIT&quantity=${qty}&price=${limitPrice}&product=${trade}&validity=DAY&disclosed_quantity=0&trigger_price=0&squareoff=0&stoploss=0&trailing_stoploss=0&user_id=${Z_USERID}`
+async function tryToPlaceOrderZerodha(limitPrice, qty, type, symbol, order_type) {
+    var data = `variety=regular&exchange=NSE&tradingsymbol=${symbol}&transaction_type=${type}&order_type=${order_type}&quantity=${qty}&price=${limitPrice}&product=${trade}&validity=DAY&disclosed_quantity=0&trigger_price=0&squareoff=0&stoploss=0&trailing_stoploss=0&user_id=${Z_USERID}`
 
     try {
+
         let result = await zerodhaCall('post', 'https://kite.zerodha.com/oms/orders/regular', data);
         result.ok = true;
         return result;
@@ -216,7 +262,7 @@ function wrap(fetchPromise) {
         fetchPromise
             .then(res => res.json())
             .then(json => {
-                resolve(json.data[0])
+                resolve(json.data[0] || json.data)
             })
             .catch(err => {
                 reject(err)
@@ -272,6 +318,86 @@ async function getWatchlist(params) {
     return body;
 }
 
+async function waitTillOrderIsOpen(orderId) {
+    return new Promise(async (resolve, reject) => {
+        let orders = await wrap(fetch("https://kite.zerodha.com/oms/orders", {
+            "headers": {
+                "accept": "application/json, text/plain, */*",
+                "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+                "authorization": "enctoken " + enctoken,
+                "if-none-match": "W/\"5tkudQ119aB023AJ\"",
+                "sec-ch-ua": "\"(Not(A:Brand\";v=\"8\", \"Chromium\";v=\"100\", \"Google Chrome\";v=\"100\"",
+                "sec-ch-ua-mobile": "?0",
+                "sec-ch-ua-platform": "\"Windows\"",
+                "sec-fetch-dest": "empty",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "x-kite-version": "2.9.10"
+            },
+            "referrer": "https://kite.zerodha.com/orders",
+            "referrerPolicy": "strict-origin-when-cross-origin",
+            "body": null,
+            "method": "GET",
+            "mode": "cors",
+            "credentials": "include"
+        }))
+
+        for (let index = 0; index < orders.length; index++) {
+            const order = orders[index];
+            if (order.order_id == orderId) {
+                if (order.status == "OPEN") {
+                    resolve(1)
+                }
+                else if (order.status == "REJECTED") {
+                    console.log('Order', orderId, 'rejected.', order.status_message)
+                    resolve(-1)
+                }
+                else {
+                    resolve(0)
+                }
+            }
+        }
+    })
+}
+
+async function startMarketWatch() {
+    let position = [{}]
+    let wsUrl = `wss://ws.zerodha.com/?api_key=kitefront&user_id=${Z_USERID}&enctoken=${encodeURI(enctoken)}&uid=1641444782679&user-agent=kite3-web&version=2.9.10`
+    wsUrl= "wss://ws.zerodha.com/?api_key=kitefront&user_id=AMC939&enctoken=DSDW8Nh%2Bv8PNG15K9CpuAhEWpyysFHTx2%2Fj0VEAHc%2B37SAdgwzFM8amCgl0wiY08sUuoxHmTWcDAIdKlBnNQQK%2Bvq%2FGAvrzmfq%2F41hqkt%2BgFDroV4UPlqg%3D%3D&uid=1641444782679&user-agent=kite3-web&version=2.9.10"
+    const WebSocket = require('ws');
+    const headers = {
+    };
+    ws = new WebSocket(wsUrl, { headers });
+    ws.binaryType = 'arraybuffer';
+    ws.on('open', () => {
+        console.log('connected', Date());
+    });
+
+    ws.on('message', function message(data) {
+        console.log('received: %s', JSON.stringify(data));
+      });
+    ws.on('close', () => {
+        console.log('disconnected', Date());
+    });
+    return;
+    let net = position.net[0]
+    console.log(net.tradingsymbol, 'x', net.quantity, net.pnl.toFixed(2))
+    let curH = moment().get('hours');
+    let curM = moment().get('m');
+    if (FORCE_SELL || curH == STOP_HRS && curM > STOP_MINS) {
+        if (net.quantity > 0) {
+            // let exitAtClose = await tryToPlaceOrderZerodha(0, net.quantity, 'SELL', symbol, "MARKET")
+            console.log('Exiting before market close', exitAtClose);
+        }
+    }
+
+    await delay(2000)
+    startMarketWatch()
+
+}
+
+
 
 
 start(stock)
+// startMarketWatch();
