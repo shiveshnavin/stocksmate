bf403a224f02928298e2bf141e10c5014a4bfe91
Shivesh Navin <shiveshnavin@gmail.com>
2022-01-24 10:42:49 +0530

    Analysis added

--- py/analysis.py
+++ py/analysis.py
@@ -1,9 +1,4 @@
 import matplotlib.pyplot as plt
-import json
-import pandas as pd
-import datetime as dt
-
-import matplotlib.pyplot as plt
 print('imported matplotlib')
 import json
 print('imported json')
@@ -11,8 +6,10 @@ import pandas as pd
 print('imported pandas')
 import datetime as dt
 print('imported datetime')
+import mplcursors
+print('imported mplcursors')
 
-f = open('data.json')
+f = open('data_minute.json')
 data = json.load(f) 
 
 df = pd.json_normalize(data)
@@ -21,6 +18,7 @@ df['date'] = df['datetime'].apply(lambda x: dt.datetime.strptime(x,"%Y-%m-%dT%H:
 df['time'] = df['datetime'].apply(lambda x: dt.datetime.strptime(x,"%Y-%m-%dT%H:%M:%S%z").strftime("%H:%M"))
 ans = [y for x, y in df.groupby('date', as_index=False)]
 
+plt.rcParams["figure.figsize"]=15,10
 
 grouped = df.groupby(df.date)
 
@@ -32,5 +30,13 @@ for group_name, df_group in grouped:
 #     print(group_name,openvalue.open)
     y = df_group['norm_close'] 
     x = df_group['time']
-    plt.plot(x, y)
-    print(df_group)    
+    ax = plt.gca()
+    temp = ax.xaxis.get_ticklabels()
+    temp = list(set(temp) - set(temp[::3]))
+    for label in temp:
+        label.set_visible(False)
+    plt.legend()
+    plt.plot(x, y, label=group_name)
+#     print(df_group)    
+mplcursors.cursor(hover=True)
+plt.show()
